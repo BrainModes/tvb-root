c49a4b446bc5ccb178d4e93d4107951594083ba7
dionperd <dionperd@gmail.com>
2025-09-22 16:18:56 +0200

    Refactored to transfer polynomial model and coupling to tvb_contrib

--- tvb_contrib/tvb/contrib/simulator/models/polynomial.py
+++ tvb_contrib/tvb/contrib/simulator/models/polynomial.py
@@ -29,6 +29,10 @@ Generic linear model.
 """
 
 import numpy
+from tvb.basic.neotraits._attr import NArray, Range
+
+from tvb_library.tvb.simulator.common import simple_gen_astr
+from tvb_library.tvb.simulator.coupling import SparseCoupling
 
 from .base import Model
 from tvb.basic.neotraits.api import NArray, List, Range
@@ -181,3 +185,102 @@ class Polynomial(Model):
         c, = coupling
         dx = self.lamda * (self.p0 + self._polyval(x)) + c + local_coupling * x[0]
         return numpy.array([dx])
+
+
+class PolynomialCoupling(SparseCoupling):
+    r"""
+        Implementing a polynomial model from
+        Kashyap, A., Geenjaar, E., Bey, P. et al.
+        Using an ordinary differential equation model to separate rest and task signals in fMRI.
+        Nat Commun 16, 7128 (2025). https://doi.org/10.1038/s41467-025-62491-6:
+        .. math:
+         p_0 + p_1 x + ... +  p_k x^k + ... + p_{n-1} x^{n-1} + p_n x^n
+        We use non integrated variables to construct the x' higher polynomial orders than 1.
+        By default, a polynomial order of 1 is assumed and default parameters are given for this case,
+        i.e., implementing a linear coupling. A first order polynomial is the minimum acceptable order.
+    """
+
+    p = NArray(
+        label=":math:`p`",
+        default=numpy.array([0.0, 1.0]),
+        domain=Range(lo=-1.0, hi=1.0, step=0.001),
+        doc="Polynomial coefficients of shape (..., M), where M is the polynomial order"
+            "i.e., the length of the last dimension should coincide with the desired order of the polynomial."
+            "The coefficients are broadcast for connections and modes if not provided explicitly. "
+            "A minimum order of 1 is assumed. Parameters p0 and p1 up to first order are set from parameter p."
+            "If parameter p of size 1 is given, the zero order coefficient is set by the p0 parameter (default = 0.0).")
+
+    p0 = NArray(
+        label=r":math:`p0`",
+        default=numpy.array([0.0]),
+        domain=Range(lo=-1.0, hi=1.0, step=0.001),
+        doc="Polynomial coefficients of zero order. "
+            "Set from parameter p, i.e., user input is ignored, unless p.size==1.")
+
+    p1 = NArray(
+        label=r":math:`p1`",
+        default=numpy.array([-1.0]),
+        domain=Range(lo=-1.0, hi=1.0, step=0.001),
+        doc="Polynomial coefficients of first order. "
+            "Set from parameter p, i.e., user input is ignored.")
+
+    parameter_names = ['p', 'p0', 'p1']
+    pre_expr = 'p0 + p1*x_j'
+    post_expr = None  # 'a * gx + b'
+    order = 1
+    p_nzw = None
+
+    def update_derived_parameters(self):
+        if self.p.ndim == 1:
+            # Assuming same polynomial for all connections and modes:
+            self.p = self.p[numpy.newaxis,  # target region
+                            numpy.newaxis,  # source region
+                            numpy.newaxis,  # modes
+                           :]               # polynomial order
+        elif self.p.ndim == 2:
+            # Assuming same polynomial for all source regions and modes:
+            self.p = self.p[:,              # target region
+                            numpy.newaxis,  # source region
+                            numpy.newaxis,  # modes
+                            :]              # polynomial order
+        elif self.p.ndim == 3:
+            # Assuming same polynomial for all modes:
+            self.p = self.p[:,              # target region
+                            :,              # source region
+                            numpy.newaxis,  # modes region
+                            :]              # polynomial order
+        correct_p0 = False
+        if self.p.shape[-1] == 1:
+            self.p = numpy.concatenate([numpy.zeros(self.p.shape[:3]+(1, )), self.p], axis=-1)
+            correct_p0 = True
+        if correct_p0:
+            self.p[:, :, 0] = self.p0
+        else:
+            self.p0 = self.p[:, :, 0]
+        self.p1 = self.p[:, :, 1]
+        # Now we have parameter p of polynomial coefficients of shape (regions, regions, modes, polynomial order)
+        self.order = int(self.p.shape[-1]) - 1
+        for j in range(2, self.p.shape[-1]):
+            # Create one extra parameter per polynomial order:
+            pj = "p%d" % j
+            self.parameter_names.append(pj)
+            setattr(self, pj, self.p[:, :, j])
+            self.pre_expr += "%s*x_j**%d" % (pj, j)
+
+    def configure(self):
+        """Set the right indirect call."""
+        super(Polynomial, self).configure()
+        self.update_derived_parameters()
+
+    def pre(self, x_i, x_j):
+        assert x_j.shape[0] == self.order
+        return self.p0 + numpy.einsum("...j,j...->...", self.p_nzw, x_j)
+
+    def __call__(self, step, history):
+        if self.p_nzw is None:
+            # To be executed only the first time to keep only nonzero weights' connections:
+            self.p_nzw = self.p[history.nnz_mask, :, :]  # new shape: (nnzw, modes, polynomial order)
+        super(Polynomial, self).__call__(step, history)
+
+    def __str__(self):
+        return simple_gen_astr(self, " ".join(self.parameter_names))
