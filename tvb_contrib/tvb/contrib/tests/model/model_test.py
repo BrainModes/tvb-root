9eae9c870abb87bb2828e766eb7d0519c5a5a7f4
dionperd <dionperd@gmail.com>
2025-09-25 18:13:00 +0200

    Added TestPolynomialModel in tvb.contrib.tests.model.model_test.py

--- tvb_contrib/tvb/contrib/tests/model/model_test.py
+++ tvb_contrib/tvb/contrib/tests/model/model_test.py
@@ -36,9 +36,14 @@ from tvb.contrib.simulator.models.larter_breakspear import LarterBreakspear
 from tvb.contrib.simulator.models.liley_steynross import LileySteynRoss
 from tvb.contrib.simulator.models.morris_lecar import MorrisLecar
 from tvb.contrib.simulator.models.wong_wang import WongWang
+from tvb.contrib.simulator.models.polynomial import Polynomial, PolynomialCoupling, polyval
+from tvb.datatypes.connectivity import Connectivity
+from tvb.simulator.simulator import Simulator
 from tvb.simulator.integrators import HeunDeterministic
+from tvb.simulator.monitors import Raw
 from tvb.tests.library.base_testcase import BaseTestCase
 from tvb.simulator.plot.phase_plane_interactive import PhasePlaneInteractive
+
 
 LOG = get_logger(__name__)
 
@@ -98,3 +103,91 @@ class TestContribModels(BaseTestCase):
 
     def test_wong_wang_model(self):
         self._show_model_figure("Wong Wang", WongWang, 2 ** -5)
+
+
+class TestPolynomialModel(BaseTestCase):
+
+    ORDER = 3
+    Ps = numpy.ones((ORDER+1, ))
+    N_MODES = 2
+
+    def _polyval(self, x, p):
+        assert numpy.allclose(polyval(x, p),
+                              numpy.sum([p[:, :, ip] * x[ip, :, :]**(ip+1) for ip in range(p.shape[-1])], axis=0),
+                              )
+
+    def _configuration(self, model, coupling, order):
+        state_variables = ("x",)
+        state_variable_mask = [True]
+        non_integrated_variables = ()
+        _nvar = 1
+        cvar = [0]
+        default_range = numpy.array([-1.0, 1.0])
+        pos_default_range = numpy.array([numpy.maximum(0.0, default_range[0]), default_range[1]])
+        state_variable_range = {"x": default_range}
+        state_variable_dfuns = {"x": "lamda * (p0 + p1*x"}
+        variables_of_interest = ('x',)
+        parameter_names = ("lamda", "p", "p0", "p1")
+        coupl_parameter_names = list(parameter_names[1:])
+        coupl_pre_expr = "p0 + p1*x_j"
+        for io in range(2, order + 1):
+            sv = "x%d" % io
+            state_variables += (sv,)
+            non_integrated_variables += (sv,)
+            state_variable_mask.append(False)
+            _nvar += 1
+            cvar.append(io - 1)
+            p = "p%d" % io
+            parameter_names += (p,)
+            state_variable_dfuns["x"] += " + %s*x^%d" % (p, io)
+            if numpy.mod(io, 2) == 0:
+                state_variable_range.update({sv: pos_default_range ** io})
+            else:
+                state_variable_range.update({sv: default_range ** io})
+            coupl_pre_expr += " + %s*x_j^%d" % (p, io)
+            coupl_parameter_names += [p]
+        state_variable_dfuns["x"] += ")"
+        cvar = numpy.array(cvar)
+        state_variable_mask = numpy.array(state_variable_mask)
+        assert state_variables == model.state_variables
+        assert numpy.all(state_variable_mask == model.state_variable_mask)
+        assert non_integrated_variables == model.non_integrated_variables
+        assert _nvar == model._nvar
+        assert variables_of_interest == model.variables_of_interest
+        for key, val in state_variable_range.items():
+            assert numpy.allclose(val, model.state_variable_range[key])
+        for key, val in state_variable_dfuns.items():
+            assert val == model.state_variable_dfuns[key]
+        assert numpy.all(cvar == model.cvar)
+        assert parameter_names == model.parameter_names
+        assert coupl_pre_expr == coupling.pre_expr
+        try:
+            assert coupl_parameter_names == coupling.parameter_names
+        except Exception as e:
+            print(order)
+            print(coupl_parameter_names)
+            print(coupling.parameter_names)
+            raise e
+
+    def _run_for_order(self, order):
+        sim = Simulator()
+        sim.connectivity = Connectivity.from_file()
+        sim.connectivity.configure()
+        sim.connectivity.tract_lengths *= 0
+        p = self.Ps[:order+1]
+        sim.coupling = PolynomialCoupling(p=p)
+        sim.model = Polynomial(p=p)
+        sim.model.number_of_modes = self.N_MODES
+        sim.integrator.dt = 0.1
+        sim.simulation_length = 0.2
+        sim.initial_conditions = numpy.ones((1, order, sim.connectivity.number_of_regions, sim.model.number_of_modes))
+        sim.monitors = (Raw(), )
+        sim.configure()
+        self._configuration(sim.model, sim.coupling, order)
+        self._polyval(sim.initial_conditions[0], sim.model.p[:, :, 1:])
+        results = sim.run()
+        assert results[0][1].shape == (2, 1, sim.connectivity.number_of_regions, sim.model.number_of_modes)
+
+    def test_polynomial_model(self):
+        for order in range(1, self.ORDER+1):
+            self._run_for_order(order)
