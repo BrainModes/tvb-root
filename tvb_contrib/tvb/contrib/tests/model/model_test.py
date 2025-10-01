0794388b6a0b951274330b9b93cd55943a53f857
dionperd <dionperd@gmail.com>
2025-10-01 19:34:03 +0200

    Added actual 1 step simulation for polynomial test in model_test

--- tvb_contrib/tvb/contrib/tests/model/model_test.py
+++ tvb_contrib/tvb/contrib/tests/model/model_test.py
@@ -39,7 +39,7 @@ from tvb.contrib.simulator.models.wong_wang import WongWang
 from tvb.contrib.simulator.models.polynomial import Polynomial, PolynomialCoupling, polyval
 from tvb.datatypes.connectivity import Connectivity
 from tvb.simulator.simulator import Simulator
-from tvb.simulator.integrators import HeunDeterministic
+from tvb.simulator.integrators import HeunDeterministic, EulerDeterministic
 from tvb.simulator.monitors import Raw
 from tvb.tests.library.base_testcase import BaseTestCase
 from tvb.simulator.plot.phase_plane_interactive import PhasePlaneInteractive
@@ -105,11 +105,65 @@ class TestContribModels(BaseTestCase):
         self._show_model_figure("Wong Wang", WongWang, 2 ** -5)
 
 
+def polydfun(x, p):
+    order = p.shape[-1] - 1
+    xp = [x]
+    for io in range(2, order + 1):
+        xp.append(x * xp[-1])
+    xp = numpy.array(xp).T
+    dx = numpy.zeros(x.shape)
+    N = x.shape[0]
+    for iX in range(N):
+        for jX in range(N):
+            # dx[iX] += p[iX, jX, 0] + numpy.sum(xp[jX] * p[iX, jX, 1:])  # explicit
+            try:
+                dx[iX] += p[iX, jX, 0] + numpy.einsum("j,j->...", xp[jX], p[iX, jX, 1:])
+            except Exception as e:
+                print(xp.shape)
+                print(p.shape)
+                print(xp[jX].shape)
+                print(p[iX, jX, 1:].shape)
+                raise e
+    return dx
+
+
+def polyint_Euler(sim, p):
+    dt = sim.integrator.dt
+    try:
+        noise = numpy.sqrt(2 * sim.integrator.noise.nsig[0].item() * dt)
+    except:
+        noise = 0
+    x = [sim.initial_conditions[-1, 0].squeeze()]
+    t = [0.0]
+    while t[-1] < sim.simulation_length:
+        dW = noise * numpy.random.normal(size=x[-1].shape) if noise else 0.0
+        x.append(x[-1] + dt * polydfun(x[-1], p) + dW)
+        t.append(t[-1] + dt)
+    return numpy.array(x), numpy.array(t)
+
+
 class TestPolynomialModel(BaseTestCase):
 
     ORDER = 3
-    Ps = numpy.ones((ORDER+1, ))
-    N_MODES = 2
+    N_MODES = 1
+
+    def __init__(self, connectivity=None, p=None):
+        if connectivity is None:
+            connectivity = Connectivity.from_file()
+            connectivity.weights = numpy.ones(connectivity.weights.shape).astype("f")
+            numpy.fill_diagonal(connectivity.weights, 0.0)
+            connectivity.tract_lengths *= 0.0
+            connectivity.configure()
+        self.connectivity = connectivity
+        Nregs = self.connectivity.number_of_regions
+        if p is None:
+            p = -3.2060285572562344e-05 + \
+                0.010215945850391128*numpy.random.normal(size=(Nregs, Nregs, self.N_MODES, self.ORDER + 1))
+        self.p = p
+        self.ORDER = self.p.shape[-1] - 1
+        self.N_MODES = self.p.shape[-2]
+        assert self.connectivity.weights.shape == self.p.shape[:2]
+        super(TestPolynomialModel, self).__init__()
 
     def _polyval(self, x, p):
         assert numpy.allclose(polyval(x, p),
@@ -163,37 +217,47 @@ class TestPolynomialModel(BaseTestCase):
         assert coupl_pre_expr == coupling.pre_expr
         assert coupl_parameter_names == coupling.parameter_names
 
-    def _run_for_order(self, order, pmode="fix"):
-        sim = Simulator()
-        sim.connectivity = Connectivity.from_file()
+    def _run_for_order(self, p):
+        order = p.shape[-1] - 1
+        sim = Simulator(connectivity=self.connectivity)
         sim.connectivity.configure()
-        if pmode == "fix":
-            p = self.Ps[:order+1]
-            pdiag = p
-        else:
-            Nregs = sim.connectivity.number_of_regions
-            p = numpy.random.normal(size=(Nregs, Nregs, self.N_MODES, order + 1))
-            pdiag = numpy.einsum("jj...->j...", p)
-            assert pdiag.shape == (Nregs, self.N_MODES, order + 1)
-            jj = numpy.arange(Nregs).astype("i")
-            p[jj, jj] = 0.0
-            assert numpy.all(p[jj, jj] == 0)
-        sim.coupling = PolynomialCoupling(p=p)
+        Nregs = sim.connectivity.number_of_regions
+        pdiag = numpy.einsum("jj...->j...", p)
+        assert pdiag.shape == (Nregs, self.N_MODES, order + 1)
+        jj = numpy.arange(Nregs).astype("i")
+        pcoupl = numpy.array(p)
+        pcoupl[jj, jj] = 0.0
+        assert numpy.all(pcoupl[jj, jj] == 0)
+        sim.coupling = PolynomialCoupling(p=pcoupl)
         sim.model = Polynomial(p=pdiag)
         sim.model.number_of_modes = self.N_MODES
         sim.model.configure()
-        sim.integrator.dt = 0.1
-        sim.simulation_length = 0.2
+        sim.integrator = EulerDeterministic(dt=0.1)
+        sim.simulation_length = 0.1
         sim.initial_conditions = \
-            numpy.random.normal(size=(1, order, sim.connectivity.number_of_regions, sim.model.number_of_modes))
+            0.1*numpy.random.normal(size=(1, order, sim.connectivity.number_of_regions, sim.model.number_of_modes))
         sim.monitors = (Raw(), )
         sim.configure()
         self._configuration(sim.model, sim.coupling, order)
         self._polyval(sim.initial_conditions[0], sim.model.p[:, :, 1:])
         results = sim.run()
-        assert results[0][1].shape == (2, 1, sim.connectivity.number_of_regions, sim.model.number_of_modes)
+        assert results[0][1].shape == (1, 1, sim.connectivity.number_of_regions, sim.model.number_of_modes)
+        if self.N_MODES == 1:
+            res = results[0][1].squeeze()
+            targres = polyint_Euler(sim, p[:, :, 0, :])[0][1:].squeeze()
+            try:
+                assert numpy.allclose(res, targres, rtol=1e-01, atol=1e-03)
+            except Exception as e:
+                print(numpy.abs(res - targres).max())
+                print(res.shape)
+                print(targres.shape)
+                print([res.min(), res.mean(), res.max()])
+                print([targres.min(), targres.mean(), targres.max()])
+                print(res)
+                print(targres)
+                raise e
 
     def test_polynomial_model(self):
         for order in range(1, self.ORDER+1):
-            self._run_for_order(order, pmode="fix")
-            self._run_for_order(order, pmode="fullrandom")
+            print("order=%d" % order)
+            self._run_for_order(self.p[:, :, :, :order+1])
